fab3be3895fcaa96ed005600695abd0f45934ecf
Célio Latorraca <celio.latorraca@corp.globo.com>
2016-01-28 18:53:57 -0200

    WiP2

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@
 class DetachPlugin extends Clappr.UICorePlugin {
   get name() { return 'detach' }
 
+  constructor(core) {
+    super(core)
+
+    this.enableMove = false
+  }
+
   // detach(e) {
   //   new Drag('queen')
   //   console.log('Drag queen')
@@ -18,31 +24,34 @@ class DetachPlugin extends Clappr.UICorePlugin {
     return this.core.containers[0].$el
   }
 
-  tryToDetach(e) {
-    debugger
-    this.core.$el.attr('class', 'queen')
-    this.draggable = new Drag(this.core.el)
-    this.draggable.init()
+  detach() {
+    console.log('detach', this.enableMove)
+    this.enableMove = true
+  }
 
-    this.getContainer().on('mouseup', ::this.unbindDrag)
+  tryToMove(e) {
+    console.log('tryToMove', this.enableMove)
+    if (this.enableMove) {
+      this.draggable = new Drag(this.core.el)
+      this.draggable.init()
+      debugger
+
+      this.getContainer().on('mouseup', ::this.unbindDrag)
+    }
   }
 
   unbindDrag(e) {
     this.draggable.destroy()
-    this.getContainer().off('mouseup', ::this.unbindDrag)
+    this.getContainer().off('mouseup')
   }
 
   render() {
+    console.log(' render')
     this.$el.html('Detach')
     this.core.$el.append(this.$el)
 
-    this.$el.on('click', ::this.tryToDetach)
-
-    // this.$el.on('click', ::this.detach)
-    // this.$el.css
-    // this.core.$el.append(this.$el)
-    //
-    // this.core.$el.attr('id', 'queen')
+    this.$el.on('click', ::this.detach)
+    this.core.$el.on('mousedown', ::this.tryToMove)
 
     return this
   }
