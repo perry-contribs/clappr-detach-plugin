a19a545209f63cdcaa980d74a07b7372d2872e55
Célio Latorraca <celio.latorraca@corp.globo.com>
2016-01-28 18:13:06 -0200

    wip1

--- example/server.js
+++ example/server.js
@@ -4,8 +4,10 @@ var app = Express();
 var server;
 
 const PATH_DIST = path.resolve(__dirname, '../dist');
+const PATH_EXAMPLE = path.resolve(__dirname, './');
 
 app.use(Express.static(PATH_DIST));
+app.use(Express.static(PATH_EXAMPLE));
 
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, './example.html'));
--- src/index.js
+++ src/index.js
@@ -1,32 +1,49 @@
-var HelloWorld = Clappr.UIContainerPlugin.extend({
-  name: 'hello_world',
-  initialize: function() {
-    this.render();
-  },
-  bindEvents: function() {
-    this.listenTo(this.container, Clappr.Events.CONTAINER_PAUSE, this.show);
-    this.listenTo(this.container, Clappr.Events.CONTAINER_PLAY, this.hide);
-  },
-  hide: function() {
-    this.$el.hide();
-  },
-  show: function() {
-    this.$el.show();
-  },
-  render: function() {
-    this.$el.html('Hello World!');
-    this.$el.css('font-size', '100px');
-    this.$el.css('color', 'white');
-    this.$el.css('background-color', 'red');
-    this.$el.css('position', 'relative');
-    this.container.$el.append(this.$el);
-    this.hide();
-    return this;
+// import { UICorePlugin, Events } from 'Clappr'
+
+// export default class DetachPlugin extends UICorePlugin {
+class DetachPlugin extends Clappr.UICorePlugin {
+  get name() { return 'detach' }
+
+  // detach(e) {
+  //   new Drag('queen')
+  //   console.log('Drag queen')
+  //   debugger
+  //   var container = this.core.containers[0]
+  //   if (container.hasPlugin('click_to_pause')) {
+  //     container.getPlugin('click_to_pause').disable()
+  //   }
+  // }
+
+  getContainer() {
+    return this.core.containers[0].$el
   }
-});
-var player = new Clappr.Player({
-  source: "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4",
-  poster: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/28/HelloWorld.svg/512px-HelloWorld.svg.png",
-  parentId: "#player",
-  plugins: { container: [HelloWorld] }
-});
+
+  tryToDetach(e) {
+    debugger
+    this.core.$el.attr('class', 'queen')
+    this.draggable = new Drag(this.core.el)
+    this.draggable.init()
+
+    this.getContainer().on('mouseup', ::this.unbindDrag)
+  }
+
+  unbindDrag(e) {
+    this.draggable.destroy()
+    this.getContainer().off('mouseup', ::this.unbindDrag)
+  }
+
+  render() {
+    this.$el.html('Detach')
+    this.core.$el.append(this.$el)
+
+    this.$el.on('click', ::this.tryToDetach)
+
+    // this.$el.on('click', ::this.detach)
+    // this.$el.css
+    // this.core.$el.append(this.$el)
+    //
+    // this.core.$el.attr('id', 'queen')
+
+    return this
+  }
+}
