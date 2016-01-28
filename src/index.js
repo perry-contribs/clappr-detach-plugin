06ca6089921ef070d39fa78cabb16744085232e7
Sergio Azevedo <sergio.azevedo@corp.globo.com>
2016-01-28 20:27:21 -0200

    fixes playerWrapper()

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@
 class DetachPlugin extends Clappr.UICorePlugin {
   get name() { return 'detach' }
 
+  playerWrapper(){
+    return this.core.$el
+  }
+
   toggleDetach() {
     this.draggable ? this.attach() : this.detach()
   }
 
   detach() {
-    this.originalStyle = playerWrapper().attr('style')
+    this.originalStyle = this.playerWrapper().attr('style')
     this.$('.detach-button').html('X attach X')
     resizePlayeAandMoveToBottomLeft()
     enablePlayerDrag()
@@ -20,7 +24,7 @@ class DetachPlugin extends Clappr.UICorePlugin {
   }
 
   enablePlayerDrag() {
-    this.draggable = new Drag(playerWrapper)
+    this.draggable = new Drag(playerWrapper())
     this.draggable.init()
   }
 
@@ -28,13 +32,13 @@ class DetachPlugin extends Clappr.UICorePlugin {
     this.draggable.destroy()
     this.draggable = null
 
-    playerWrapper.attr('style', this.originalStyle)
+    this.playerWrapper().attr('style', this.originalStyle)
     this.$('.detach-button').html('\\/ detach \\/')
   }
 
   render() {
     this.$el.html('<span class="detach-button">\\/ Detach \\/</span>')
-    playerWrapper.append(this.$el)
+    this.playerWrapper().append(this.$el)
 
     this.$el.on('click', ::this.toggleDetach)
 
