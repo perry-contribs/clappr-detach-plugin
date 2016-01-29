c4b3259c861c91010855a257259ba30bddaa1077
Sergio Azevedo <sergio.azevedo@corp.globo.com>
2016-01-29 14:37:14 -0200

    changes example video and poster

--- example/example.js
+++ example/example.js
@@ -1,6 +1,6 @@
 var player = new Clappr.Player({
-  source: "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4",
-  poster: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/28/HelloWorld.svg/512px-HelloWorld.svg.png",
+  source: "https://voddownload01.video.globo.com/v0/04/c1/f2/2143530_f20b11011d7a52b6aa38b4c6a543d0b0682adccc/2143530-web360.mp4?h=0502145408536615039439701454088966204395320711mJeq2VlTp8xq_fXSy-wRQ&k=html5",
+  poster: "http://s04.video.glbimg.com/x360/2143530.jpg",
   parentId: "#player",
   plugins: {
     core: [ DetachPlugin ]
