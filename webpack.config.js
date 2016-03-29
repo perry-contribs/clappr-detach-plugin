1e39f530e7001e07726c839527a0aac68f9a976f
Marcos de Oliveira Couto Filho <marcos.ocf01@gmail.com>
2016-03-28 23:44:27 -0300

    Create a simple build with webpack

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src/index.js'),
+  module: {
+    loaders: [
+      {
+        test: /src\/.+.js$/,
+        loader: 'babel',
+        query: {
+          compact: true,
+        }
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['css', "sass"]
+      },
+      {
+        test: /\.html/, loader: 'html?minimize=true'
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['', '.js'],
+  },
+  externals: {
+    "Clappr": "Clappr",
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'clappr-detach-plugin.js',
+    library: 'ClapprDetachPlugin',
+    libraryTarget: 'umd',
+  }
+};
