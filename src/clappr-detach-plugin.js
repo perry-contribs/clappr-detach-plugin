8649f35ce5b73fe9a8a20a3cc1c1f769e1efff3f
Célio Latorraca <celio.latorraca@corp.globo.com>
2016-04-07 21:46:36 -0300

    Add all logic to detach and atach when clicking toggle buttons

--- src/clappr-detach-plugin.js
+++ src/clappr-detach-plugin.js
@@ -36,32 +36,41 @@ export default class ClapprDetachPlugin extends UICorePlugin {
 
   get playerWrapper() { return this.core.$el }
   get mediaControl() { return this.core.mediaControl }
-  get clickToPausePlugin() { return this.core.containers[0].getPlugin('click_to_pause') }
   get seekBarContainer() { return this.mediaControl.$el.find('.media-control-center-panel') }
+  get clickToPausePlugin() { return this.core.containers[0].getPlugin('click_to_pause') }
+
   get poster() { return this.core.options.poster ? this.core.options.poster : 'data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs=' }
   get attributes() {
     return {
-      'class': 'clappr-detach',
-      'data-detach-selector': ''
+      'class': 'clappr-detach'
     }
   }
 
   bindEvents() {
+    this.listenTo(this.core, Events.CORE_READY, this.onCoreReady)
     this.listenTo(this.mediaControl, Events.MEDIACONTROL_RENDERED, this.onMediaControlRendered)
   }
 
   render() {
-    this.renderPlaceholder()
-    this.renderStylesheet()
-    this.appendStatics()
-
-    this.core.$el.append(this.el)
+    this.createPlaceholder()
+    this.createStylesheet()
 
     return this
   }
 
-  renderPlaceholder() {
+  createPlaceholder() {
     this.$el.html(this.placeholderMarkup)
+
+    this.$el.find('.clappr-detach__placeholder-icon').on('click', ::this.toggleDetach)
+  }
+
+  createStylesheet() {
+    const detachStyle = Styler.getStyleFor(DetachStyle)
+    this.$el.append(detachStyle)
+  }
+
+  appendStatics() {
+    this.playerWrapper.parent().append(this.el)
   }
 
   renderMediaControlButton() {
@@ -71,13 +80,8 @@ export default class ClapprDetachPlugin extends UICorePlugin {
     rightPanel.append(this.mediaControlButtonMarkup)
   }
 
-  renderStylesheet() {
-    const detachStyle = Styler.getStyleFor(DetachStyle)
-    this.$el.append(detachStyle)
-  }
-
-  appendStatics() {
-    this.core.$el.append(this.el)
+  onCoreReady() {
+    this.appendStatics()
   }
 
   onMediaControlRendered() {
@@ -85,115 +89,88 @@ export default class ClapprDetachPlugin extends UICorePlugin {
     this.mediaControlDetachButton.on('click', ::this.toggleDetach)
   }
 
-  //
-  // resizeAndRepositionPlayer() {
-  //   // this.addPlaceholder()
-  //   this.hidePlayer()
-  //   this.enablePlayerDrag()
-  //   this.enableMiniPlayer()
-  // }
-  //
-  // hidePlayer() {
-  //   this.playerWrapper.css({
-  //     opacity: 0
-  //   })
-  // }
-  //
-  // enablePlayerDrag() {
-  //   this.disablePauseClick()
-  //   this.draggable = new Drag(this.playerWrapper[0])
-  //   this.draggable.init()
-  // }
-  //
-  // disablePlayerDrag() {
-  //   this.enablePauseClick()
-  //   this.draggable.destroy()
-  //   this.draggable = null
-  // }
-  //
-  // enablePauseClick() {
-  //   this.clickToPausePlugin.enable()
-  // }
-  //
-  // disablePauseClick() {
-  //   this.clickToPausePlugin.disable()
-  // }
-  //
-  // enableMiniPlayer() {
-  //   this.hideSeekBar()
-  //   this.playerWrapper.css({
-  //     transition: 'opacity 1s ease',
-  //     transition: 'transform 0.5s ease-in-out',
-  //     transform: 'translateY(-130px)',
-  //     opacity: 1,
-  //     height: '180px',
-  //     width: '320px',
-  //     left: '10px',
-  //     bottom: '-100px',
-  //     zIndex: '99999'
-  //   })
-  // }
-  //
-  // disableMiniPlayer() {
-  //   this.playerWrapper.attr('style', this.originalStyle)
-  //   this.showSeekBar()
-  // }
-  //
-  // attach() {
-  //   this.disablePlayerDrag()
-  //   this.removePlaceholder()
-  //   this.disableMiniPlayer()
-  //   // this.mediaControl.$('.detach-button').html('\\/ detach \\/')
-  // }
-  //
-  // detach() {
-  //   this.originalStyle = this.playerWrapper.attr('style')
-  //   this.resizeAndRepositionPlayer()
-  // }
-  //
-  // addPlaceholder() {
-  //   let placeholder = document.createElement('div')
-  //
-  //   placeholder.setAttribute('style', this.originalStyle)
-  //   placeholder.setAttribute('class', 'video-placeholder')
-  //   placeholder.style.display = 'flex'
-  //   placeholder.style.justifyContent = 'center'
-  //   placeholder.style.alignItems = 'center'
-  //   placeholder.style.position = 'relative'
-  //
-  //   const button = this.placeholderDetachButton()
-  //   if (document.attachEvent) {
-  //     button.attachEvent('onclick', ::this.toggleDetach)
-  //   } else {
-  //     button.addEventListener('click', ::this.toggleDetach)
-  //   }
-  //
-  //   let css = document.createElement('style')
-  //   css.class = 'clappr-style'
-  //   css.innerHTML = '.video-placeholder:before { content:""; position:absolute; width:100%; height:100%; background:inherit; z-index:-1; -webkit-filter: blur(5px); -moz-filter: blur(5px); -o-filter: blur(5px); -ms-filter: blur(5px); filter: blur(5px); top:0; left:0; opacity: 0.85; background-size:100% 100%; background-image:'+ 'url(' + this.poster + ');' +' }'
-  //
-  //   placeholder.appendChild(css)
-  //   placeholder.appendChild(button)
-  //   this.playerWrapper.parent().prepend(placeholder)
-  // }
-  //
-  // removePlaceholder() {
-  //   this.playerWrapper.siblings('.video-placeholder').remove()
-  // }
-  //
-  // insertButton() {
-  //   this.mediaControl.$el.find('.media-control-right-panel').append(this.$el)
-  // }
-  //
-  // showSeekBar() {
-  //   this.seekBarContainer.show()
-  // }
-  //
-  // hideSeekBar() {
-  //   this.seekBarContainer.hide()
-  // }
+  resizeAndRepositionPlayer() {
+    this.hidePlayer()
+    this.enablePlayerDrag()
+    this.enableMiniPlayer()
+  }
+
+  hidePlayer() {
+    this.playerWrapper.css({
+      opacity: 0
+    })
+  }
+
+  enablePlayerDrag() {
+    this.disablePauseClick()
+    this.draggable = new Drag(this.playerWrapper[0])
+    this.draggable.init()
+  }
+
+  disablePlayerDrag() {
+    this.enablePauseClick()
+    this.draggable.destroy()
+    this.draggable = null
+  }
+
+  enablePauseClick() {
+    this.clickToPausePlugin.enable()
+  }
+
+  disablePauseClick() {
+    this.clickToPausePlugin.disable()
+  }
+
+  enableMiniPlayer() {
+    this.hideSeekBar()
+    this.playerWrapper.css({
+      transition: 'opacity 1s ease',
+      transition: 'transform 0.5s ease-in-out',
+      transform: 'translateY(-130px)',
+      opacity: 1,
+      height: '180px',
+      width: '320px',
+      left: '10px',
+      bottom: '-100px',
+      zIndex: '99999'
+    })
+  }
+
+  disableMiniPlayer() {
+    this.playerWrapper.attr('style', this.originalStyle)
+    this.showSeekBar()
+  }
+
+  hidePlaceholder() {
+    this.$el.removeClass('clappr-detach--visible')
+  }
+
+  showPlaceholder() {
+    this.$el.attr('style', this.originalStyle)
+    this.$el.addClass('clappr-detach--visible')
+  }
+
+  showSeekBar() {
+    this.seekBarContainer.show()
+  }
+
+  hideSeekBar() {
+    this.seekBarContainer.hide()
+  }
 
   toggleDetach() {
     this.draggable ? this.attach() : this.detach()
+  }
+
+  attach() {
+    this.disablePlayerDrag()
+    this.hidePlaceholder()
+    this.disableMiniPlayer()
+  }
+
+  detach() {
+    this.originalStyle = this.playerWrapper.attr('style')
+    this.resizeAndRepositionPlayer()
+    this.showPlaceholder()
   }
 }
